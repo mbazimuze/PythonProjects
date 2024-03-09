e5584ab9679c027493eb98e119dcb261fa8f27fa
mbazimuze <mbazimuze@gmail.com>
2024-03-09 14:58:52 +0300

    One line added

---
+++ PythonIfelse.py
@@ -0,0 +1,16 @@
+x = int(input("Please enter an integer: "))
+
+# An if … elif … elif … sequence is a substitute for the switch or case statements found in other languages.
+# ‘elif’ is short for ‘else if’, and is useful to avoid excessive indentation and looping
+
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+print("Here we are")
