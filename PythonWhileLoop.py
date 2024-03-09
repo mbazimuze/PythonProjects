7f9dc0d670aad3d2a34152a68affc8eadb87f942
mbazimuze <mbazimuze@gmail.com>
2024-03-09 14:40:04 +0300

    One line added

---
+++ PythonWhileLoop.py
@@ -0,0 +1,17 @@
+# Fibonacci series:
+# the sum of two elements defines the next
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a+b
+
+# Second while loop with an end to avoid the newline after the output, or end the output with a different string
+
+a, b = 0, 1
+while a < 1000:
+     print(a, end=',')
+     a, b = b, a + b
+
+print("\n")
+
+print("Here we are")
